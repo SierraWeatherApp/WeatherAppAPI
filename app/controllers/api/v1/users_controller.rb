2128d83b8dd6d3c09a7d29ad00ea9d2682a05331
Felipe Oliver <felioliver96@gmail.com>
2023-04-19 13:25:23 +0200

    added users, users model, users test

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,31 @@
-class UsersController < ApplicationController
-  def create
-    # TODO
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def create
+        response = { message: nil, status: :created }
+        begin
+          valid_params?(%i[device_id])
+          User.create!(device_id: permitted_params[:device_id])
+        rescue Errors::MissingArgumentError => e
+          response = { message: e, status: :bad_request }
+        rescue StandardError => e
+          response = { message: e, status: :internal_server_error }
+        end
+        render json: response[:message], status: response[:status]
+      end
+
+      private
+
+      def permitted_params
+        params.permit(:device_id)
+      end
+
+      def valid_params?(parameters = [])
+        parameters.each do |param|
+          raise Errors::MissingArgumentError, param unless params[param]
+        end
+      end
+    end
+
   end
 end
