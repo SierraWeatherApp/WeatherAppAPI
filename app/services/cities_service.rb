0a2b73501db0f73bb4a5a8ff56a96cecc85a9c2f
Tim <sonnenpelzxx@gmail.com>
2023-04-20 09:38:36 +0200

    corrected syntax errors

--- app/services/cities_service.rb
+++ app/services/cities_service.rb
@@ -1,33 +1,32 @@
 class CitiesService
-    def create_city(user, city_params)
-        cities = City.filter_by_user(user)
-        city = City.create!(city_id: city_params[:city_id], 
-            city_name: city_params[:city_name], 
-            country: city_params[:country], 
-            latitude: city_params[:latitude], 
-            longitude: city_params[:longitude],
-            order: cities.length + 1,
-            user_id: user.id
-        )
+  def create_city(user, city_params)
+    cities = City.filter_by_user(user)
+    City.create!(city_id: city_params[:city_id],
+                 city_name: city_params[:city_name],
+                 country: city_params[:country],
+                 latitude: city_params[:latitude],
+                 longitude: city_params[:longitude],
+                 order: cities.length + 1,
+                 user_id: user.id)
+  end
+
+  def delete_city(city)
+    city.destroy
+  end
+
+  def change_order_cities(params)
+    cities = params[:cities]
+    cities.each do |city|
+      change_order_city(city[:id], city[:order])
     end
-    def delete_city(city)
-        begin 
-            city.destroy
-            rescue => exception
-                raise exception
-        end
-    end
-    def change_order_cities(params)
-        cities = params[:cities]
-        cities.each do |city| 
-            change_order_city(city[:id], city[:order]) 
-        end
-    end
-    def change_order_city(id, order)
-        city = City.find(id)
-        city.update(order: order)
-    end
-    def sort_cities_by_order(cities)
-        cities.sort_by{|city| city.order}
-    end
+  end
+
+  def change_order_city(id, order)
+    city = City.find(id)
+    city.update(order:)
+  end
+
+  def sort_cities_by_order(cities)
+    cities.sort_by(&:order)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,10 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: %i[create] do
         put '/cities/city_order' => 'cities#city_order'
-        resources :cities, only: %i[create show index] do 
+        resources :cities, only: %i[create show index] do
           delete '/:id', action: :destroy, on: :collection
-          #get '/:id', action: :show, on: :collection
+          # get '/:id', action: :show, on: :collection
         end
-
       end
     end
   end
