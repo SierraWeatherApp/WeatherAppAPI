9ff599a7eb2db75500dfefabb126f43842f12b58
Felipe Oliver <felioliver96@gmail.com>
2023-04-20 16:11:59 +0200

    change some test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User do
+  let(:device_id) { '19238723y7dh3su2as21dfs231a213sd2af' }
+
   it 'is valid with valid attributes' do
-    expect(described_class.new(device_id: '19238723y7dh3su2as21dfs231a213sd2af')).to be_valid
+    expect(described_class.new(device_id:)).to be_valid
   end
 
   it 'is invalid without device_id' do
