388af07d442e473e2abf626368a92ad40e67e4ab
Aditya-1301 <agupta0595@gmail.com>
2023-04-24 15:32:38 +0200

    Update weathers_services.rb

--- app/services/weathers_services.rb
+++ app/services/weathers_services.rb
@@ -2,6 +2,7 @@
 
 require 'json'
 class WeathersServices
+  # rubocop:disable Metrics/AbcSize
   def retrieve_values(params)
     jsonresponse = {}
     data_json = JSON.parse(current_weather(params[:latitude], params[:longitude]))
@@ -14,9 +15,9 @@ class WeathersServices
     if params.key?(:relativehumidity_2m) && params[:relativehumidity_2m] == 'true'
       jsonresponse = jsonresponse.merge({ relativehumidity_2m: data_json['relativehumidity_2m'][Time.now.hour] })
     end
-    byebug
     jsonresponse
   end
+  # rubocop:enable Metrics/AbcSize
 
   def current_weather(latitude, longitude)
     url = "https://api.open-meteo.com/v1/forecast?latitude=#{latitude}&longitude=#{longitude}&hourly=relativehumidity_2m&current_weather=true&forecast_days=1&windspeed_unit=ms"
