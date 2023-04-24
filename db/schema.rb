680c8dfc99dfc1d2313d24bd62679c77455617e7
QBigTree <nicholas.lawrence.james@gmail.com>
2023-04-24 10:06:41 +0200

    Default settings

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_418_135_128) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_135128) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'cities', force: :cascade do |t|
-    t.bigint 'user_id'
-    t.float 'latitude', default: 0.0
-    t.float 'longitude', default: 0.0
-    t.integer 'city_id'
-    t.string 'city_name'
-    t.string 'country'
-    t.integer 'order', default: 0
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_cities_on_user_id'
+  create_table "cities", force: :cascade do |t|
+    t.bigint "user_id"
+    t.float "latitude", default: 0.0
+    t.float "longitude", default: 0.0
+    t.integer "city_id"
+    t.string "city_name"
+    t.string "country"
+    t.integer "order", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cities_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'device_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "device_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
