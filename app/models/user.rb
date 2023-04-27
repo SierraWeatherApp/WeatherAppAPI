6407655a5e703013e0e077ac487d91dba4ab4e0f
QBigTree <nicholas.lawrence.james@gmail.com>
2023-04-27 15:24:47 +0200

    merge with main

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,18 @@ class User < ApplicationRecord
   validates_presence_of :device_id
   validates_uniqueness_of :device_id
   validate :duplicate_check
-
+  validate :temperature_units
   private
 
   def duplicate_check
     return if cities_ids.uniq.length == cities_ids.length
 
     errors.add(:cities_ids, 'repeating_entry')
+  end
+
+  def temperature_units
+    return if temp_units == "C" || temp_units == "F"
+
+    errors.add(:temp_units, 'incorrect_temp_format')
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,4 +18,8 @@ RSpec.describe User do
   it 'is invalid to repeat the city_id for a specific user' do
     expect(described_class.new(cities_ids: user_test.cities_ids.push(3))).not_to be_valid
   end
+
+  it 'is invalid to send incorrect temperature format' do
+    expect(described_class.new(device_id: '19238723y7dh3su2as21dfs231a213sd2ag', temp_units: "f")).not_to be_valid
+  end
 end
