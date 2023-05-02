88fd466d9f6b2cd092b49a754b2db774a81ccae9
Aditya-1301 <agupta0595@gmail.com>
2023-05-02 09:33:57 +0200

    Update weathers_controller_spec.rb

--- spec/requests/api/v1/weathers_controller_spec.rb
+++ spec/requests/api/v1/weathers_controller_spec.rb
@@ -31,14 +31,14 @@ RSpec.describe Api::V1::WeathersController do
       expect(JSON.parse(response.body)['windspeed']).to be_a(Float)
     end
 
-    it 'returns humidity to be nil' do
-      expect(JSON.parse(response.body)['relativehumidity_2m']).to be_nil
-    end
-
     it 'returns is_day to be nil' do
       expect(JSON.parse(response.body)['is_day']).to be_nil
     end
 
+    # it 'returns humidity to be nil' do
+    #   expect(JSON.parse(response.body)['relativehumidity_2m']).to be_nil
+    # end
+    #
     # it 'returns apparent temperature' do
     #   expect(JSON.parse(response.body)['apparent_temperature']).to be_a(Float)
     # end
