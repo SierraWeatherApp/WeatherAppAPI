a769ad39d24c8ffdcef13637f196c553ce82b1e3
QBigTree <nicholas.lawrence.james@gmail.com>
2023-05-03 08:49:28 +0200

    Shortened code and change function's name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,7 @@ class User < ApplicationRecord
   validates_presence_of :device_id
   validates_uniqueness_of :device_id
   validates :look, numericality: true, comparison: { greater_than_or_equal_to: 0 }
-  validate :duplicate_check
-  validate :temperature_units
-  validate :gender_name
+  validate :duplicate_check, :temperature_unit, :gender_name
 
   private
 
@@ -14,7 +12,7 @@ class User < ApplicationRecord
     errors.add(:cities_ids, 'repeating_entry')
   end
 
-  def temperature_units
+  def temperature_unit
     return if temp_unit == 'celsius' || temp_unit == 'fahrenheit'
 
     errors.add(:temp_unit, 'incorrect_temp_format')
