ab0cc36f39681da49559d0a0ffc8a649841daeea
QBigTree <nicholas.lawrence.james@gmail.com>
2023-05-08 15:35:37 +0200

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,45 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_508_071_112) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_08_071112) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'cities', force: :cascade do |t|
-    t.bigint 'weather_id'
-    t.string 'name'
-    t.string 'country'
-    t.float 'latitude'
-    t.float 'longitude'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "cities", force: :cascade do |t|
+    t.bigint "weather_id"
+    t.string "name"
+    t.string "country"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'cloth_types', force: :cascade do |t|
-    t.string 'name'
-    t.string 'section'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "cloth_types", force: :cascade do |t|
+    t.string "name"
+    t.string "section"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'questions', force: :cascade do |t|
-    t.string 'question'
-    t.integer 'label'
-    t.integer 'min', default: 0
-    t.integer 'max', default: 1
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "questions", force: :cascade do |t|
+    t.string "question"
+    t.integer "label"
+    t.integer "min", default: 0
+    t.integer "max", default: 1
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'device_id'
-    t.string 'temp_unit', default: 'celsius'
-    t.string 'gender', default: 'female'
-    t.integer 'look', default: 0
-    t.integer 'cities_ids', default: [], array: true
-    t.json 'answers', default: {}
-    t.json 'preferences', default: {}
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "device_id"
+    t.string "temp_unit", default: "celsius"
+    t.string "gender", default: "female"
+    t.integer "look", default: 0
+    t.integer "cities_ids", default: [], array: true
+    t.json "answers", default: {}
+    t.json "preferences", default: {}
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
