5a5acb052e8cfaa315d7b4c42d1658e7300dac3d
QBigTree <nicholas.lawrence.james@gmail.com>
2023-05-09 10:17:48 +0200

    Logic discovered
    Since there is automatic creation then consequent deletion will do the job of defending sql database from faulty device_ids. Hence, test cases were removed for checking it. Also one line reduction inside weather_controller class

--- spec/requests/api/v1/users_controller_spec.rb
+++ spec/requests/api/v1/users_controller_spec.rb
@@ -531,17 +531,17 @@ RSpec.describe 'Users' do
   end
 
   describe 'DELETE user information' do
-    context 'when user exists' do
-      let(:device_id) { 'k123v23hj213321jh12kj3123k' }
-      let(:city_st) do
-        create(:city, name: 'Stockholm', weather_id: 2_673_730, country: 'Sweden', latitude: 59.33459,
-                      longitude: 18.06324)
-      end
-      let(:city_bs) do
-        create(:city, name: 'Buenos Aires', weather_id: 3_435_910, country: 'Argentina', latitude: -34.61315,
-                      longitude: -58.37723)
-      end
+    let(:device_id) { 'k123v23hj213321jh12kj3123k' }
+    let!(:city_st) do
+      create(:city, name: 'Stockholm', weather_id: 2_673_730, country: 'Sweden', latitude: 59.33459,
+             longitude: 18.06324)
+    end
+    let!(:city_bs) do
+      create(:city, name: 'Buenos Aires', weather_id: 3_435_910, country: 'Argentina', latitude: -34.61315,
+             longitude: -58.37723)
+    end
 
+    context 'when user exists' do
       before do
         create(:user, device_id:, cities_ids: [city_bs.id, city_st.id])
         delete '/api/v1/user',
@@ -558,19 +558,6 @@ RSpec.describe 'Users' do
 
       it 'allows to create a new user with the same device id' do
         expect(create(:user, device_id:, cities_ids: [city_bs.id, city_st.id])).to be_valid
-      end
-    end
-
-    context 'when user does not exist' do
-      let(:device_id) { 'k123v23hj213321jh12kj3123k' }
-
-      before do
-        delete '/api/v1/user',
-               headers: { 'x-device-id' => device_id }
-      end
-
-      it 'returns internal_server error' do
-        expect(response).to have_http_status(:internal_server_error)
       end
     end
   end
